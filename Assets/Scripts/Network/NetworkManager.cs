5e1bc409a91c44aab89b3960d36e71ad898b6fda
Nico <nicogreco4k@gmail.com>
2016-08-04 22:52:33 -0400

    Player spawns at a spawn point on FPS level

--- Assets/Scripts/Network/NetworkManager.cs
+++ Assets/Scripts/Network/NetworkManager.cs
@@ -3,6 +3,8 @@ using System.Collections;
 
 public class NetworkManager : MonoBehaviour {
 
+    public Camera standbyCam;
+
 	// Use this for initialization
 	void Start ()
     {
@@ -21,7 +23,6 @@ public class NetworkManager : MonoBehaviour {
         {
             GUILayout.Label(game.name + " " + game.playerCount + "/" + game.maxPlayers);
         }
-
     }
 
     void OnJoinedLobby()
@@ -34,12 +35,19 @@ public class NetworkManager : MonoBehaviour {
     void OnJoinedRoom()
     {
         Debug.Log("<color=green>Successful connection to Room established</color>");
+        SpawnPlayer();
     }
 
     void OnPhotonRandomJoinFailed(object[] codeAndMsg)
     {
         Debug.Log("<color=red>Error: " + codeAndMsg[0] + ", " + codeAndMsg[1] +"</color>");
         PhotonNetwork.CreateRoom(null);
+    }
+
+    void SpawnPlayer()
+    {
+        PhotonNetwork.Instantiate("FPSPlayer", new Vector3(-75, 14, 0), Quaternion.identity, 0);
+        standbyCam.enabled = false;
     }
 
 }
